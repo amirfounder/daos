05c4d1c65f4257005ad6f8729ad9e61b5fbfa2c4
Amir Sharapov <amir@amirfounder.com>
2022-04-18 23:41:23 -0500

    updated defaults for idnex props

--- daos/internal/news_article/html_doc_index/model.py
+++ daos/internal/news_article/html_doc_index/model.py
@@ -1,5 +1,3 @@
-import datetime
-
 from sqlalchemy import Column, BigInteger, DateTime, String
 
 from daos.base import BaseDatabaseModel as Base
@@ -8,7 +6,7 @@ from daos.base import BaseDatabaseModel as Base
 class NewsArticleHtmlDocumentIndexModel(Base):
     __tablename__ = 'news_article_html_document_index'
 
-    retrieved_from_web_at = Column(DateTime(True), default=datetime.datetime.utcnow)
+    retrieved_from_web_at = Column(DateTime(True))
     document_id = Column(BigInteger)
     document_format = Column(String)
 
@@ -16,9 +14,9 @@ class NewsArticleHtmlDocumentIndexModel(Base):
     html_only_version_document_path = Column(String)
     no_js_version_document_path = Column(String)
 
-    is_raw_version_stored = Column(String)
-    is_html_only_version_stored = Column(String)
-    is_no_js_version_stored = Column(String)
+    is_raw_version_stored = Column(String, default=False)
+    is_html_only_version_stored = Column(String, default=False)
+    is_no_js_version_stored = Column(String, default=False)
 
     source_url = Column(String)
     search_keywords = Column(String)
