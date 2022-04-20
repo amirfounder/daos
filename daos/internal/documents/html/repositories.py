7fa72e965671c05a22e06fd095562f9740aa5b78
Amir Sharapov <amir@amirfounder.com>
2022-04-20 02:22:49 -0500

    fix

--- daos/internal/documents/html/repositories.py
+++ daos/internal/documents/html/repositories.py
@@ -1,38 +1,38 @@
-from daos.abstracts.document.html.repository import BaseHtmlDocRepository as DocBase
-from daos.abstracts.document.html.model import HtmlDocumentModel
-from daos.abstracts.database.repository import BaseDBRepository as DBBase
+from daos.abstracts.document.html.repository import BaseHtmlDocRepository as DocRepository
+from daos.abstracts.document.html.model import HtmlDocumentModel as DocModel
+from daos.abstracts.database.repository import BaseDBRepository as DBRepository
 from daos.internal.base.doc_index_model import HtmlDocumentIndexItem
 from daos.internal.paths import Paths
 
 
-class GoogleSearchResultsHtmlDocumentNoJSRepository(DocBase):
+class GoogleSearchResultsHtmlDocumentNoJSRepository(DocRepository[DocModel]):
     def __init__(self):
-        super().__init__(HtmlDocumentModel, Paths.GOOGLE_SEARCH_RESULTS_NO_JS_DIR_PATH.value)
+        super().__init__(DocModel, Paths.GOOGLE_SEARCH_RESULTS_NO_JS_DIR_PATH.value)
 
-class GoogleSearchResultsHtmlDocumentRawRepository(DocBase):
+class GoogleSearchResultsHtmlDocumentRawRepository(DocRepository[DocModel]):
     def __init__(self):
-        super().__init__(HtmlDocumentModel, Paths.GOOGLE_SEARCH_RESULTS_RAW_DIR_PATH.value)
+        super().__init__(DocModel, Paths.GOOGLE_SEARCH_RESULTS_RAW_DIR_PATH.value)
 
-class GoogleSearchResultsHtmlDocumentHtmlOnlyRepository(DocBase):
+class GoogleSearchResultsHtmlDocumentHtmlOnlyRepository(DocRepository[DocModel]):
     def __init__(self):
-        super().__init__(HtmlDocumentModel, Paths.GOOGLE_SEARCH_RESULTS_HTML_ONLY_DIR_PATH.value)
+        super().__init__(DocModel, Paths.GOOGLE_SEARCH_RESULTS_HTML_ONLY_DIR_PATH.value)
 
-class GoogleSearchResultsHtmlDocumentIndexRepository(DBBase):
+class GoogleSearchResultsHtmlDocumentIndexRepository(DBRepository[HtmlDocumentIndexItem]):
     def __init__(self):
         super().__init__(HtmlDocumentIndexItem)
 
-class NewsArticleHtmlDocumentNoJSRepository(DocBase):
+class NewsArticleHtmlDocumentNoJSRepository(DocRepository[DocModel]):
     def __init__(self):
-        super().__init__(HtmlDocumentModel, Paths.NEWS_ARTICLE_NO_JS_DIR_PATH.value)
+        super().__init__(DocModel, Paths.NEWS_ARTICLE_NO_JS_DIR_PATH.value)
 
-class NewsArticleHtmlDocumentRawRepository(DocBase):
+class NewsArticleHtmlDocumentRawRepository(DocRepository[DocModel]):
     def __init__(self):
-        super().__init__(HtmlDocumentModel, Paths.NEWS_ARTICLE_RAW_DIR_PATH.value)
+        super().__init__(DocModel, Paths.NEWS_ARTICLE_RAW_DIR_PATH.value)
 
-class NewsArticleHtmlDocumentHtmlOnlyRepository(DocBase):
+class NewsArticleHtmlDocumentHtmlOnlyRepository(DocRepository[DocModel]):
     def __init__(self):
-        super().__init__(HtmlDocumentModel, Paths.NEWS_ARTICLE_HTML_ONLY_DIR_PATH.value)
+        super().__init__(DocModel, Paths.NEWS_ARTICLE_HTML_ONLY_DIR_PATH.value)
 
-class NewsArticleHtmlDocumentIndexRepository(DBBase):
+class NewsArticleHtmlDocumentIndexRepository(DBRepository[HtmlDocumentIndexItem]):
     def __init__(self):
         super().__init__(HtmlDocumentIndexItem)
