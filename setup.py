ce7c06f3085e3e09866011c54b3306384acbfe3f
Amir Sharapov <amir@amirfounder.com>
2022-04-20 00:37:24 -0500

    setup py fix

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='daos',
-    version='0.0.27',
+    version='0.0.28',
     packages=find_packages(),
     install_requires=[
         'SQLAlchemy==1.4.35',
         'psycopg2==2.9.3',
-        'bs4~=0.0.1'
+        'bs4~=0.0.1',
+        'Inflector~=3.0.1'
     ]
 )
