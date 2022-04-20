a02e98f07a51b9e6fad9d8c44d6b73791848ebad
Amir Sharapov <amir@amirfounder.com>
2022-04-20 00:27:33 -0500

    ver bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='daos',
-    version='0.0.24',
+    version='0.0.25',
     packages=find_packages(),
     install_requires=[
         'SQLAlchemy==1.4.35',
