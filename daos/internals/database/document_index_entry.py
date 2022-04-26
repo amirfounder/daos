c276c256d2904165478329c249c5def2719d6b1d
Amir Sharapov <amir@amirfounder.com>
2022-04-26 16:46:00 -0500

    changes observed is int

--- daos/internals/database/document_index_entry.py
+++ daos/internals/database/document_index_entry.py
@@ -10,5 +10,5 @@ class DocumentIndexEntry(Model):
     url = Column(String)
     session_id = Column(String)
     ms_elapsed_on_webpage = Column(Integer)
-    page_changes_observed = Column(Boolean, nullable=False, default=False)
+    page_changes_observed = Column(Integer)
     document_path = Column(String)
