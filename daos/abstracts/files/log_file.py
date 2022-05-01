2a9707f89ce6e9bf2516245a1cd69f0ec2149104
Amir Sharapov <amir@amirfounder.com>
2022-05-01 11:25:20 -0500

    git log handles logic

--- daos/abstracts/files/log_file.py
+++ daos/abstracts/files/log_file.py
@@ -1,18 +1,21 @@
-import warnings
-
 from daos.abstracts.files.base import File
 
 
 class LogFile(File):
     suffix = '.log'
 
-    def __init__(self):
-        super().__init__(path=self.last_file_path)
+    def __init__(self, path: str = None):
+        if not path:
+            path = self.last_file_path
+        super().__init__(path=path)
 
-    def log(self, message: str):
-        if self.exceeds_max_file_size:
-            warnings.warn(f"Max file size exceeded for log file: {self.dir_path}")
+    @classmethod
+    def log(cls, message: str):
+        instance = cls()
+        if instance.exceeds_max_file_size:
+            # noinspection PyTypeChecker
+            instance = cls(path=cls.next_file_path)
 
-        self.load()
-        self.contents += message + '\n'
-        self.flush()
+        instance.load()
+        instance.contents += message + '\n'
+        instance.flush()
